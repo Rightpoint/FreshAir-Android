e4bb5b26db7e9a6fb043d97c53e9ee9043c8567d
Dylan James <dylanrjames@gmail.com>
2016-02-03 12:40:28 -0500

    Update button goes to Play Store

--- FreshAir/src/main/java/com/raizlabs/freshair/FreshAir.java
+++ FreshAir/src/main/java/com/raizlabs/freshair/FreshAir.java
@@ -2,7 +2,14 @@ package com.raizlabs.freshair;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 
 public class FreshAir {
@@ -29,13 +36,24 @@ public class FreshAir {
 
         foregroundActivityTracker.postToForegroundActivity(new ForegroundActivityTracker.ActivityAction() {
             @Override
-            public void execute(Activity activity) {
+            public void execute(final Activity activity) {
                 final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity)
                         .setTitle(forced ? updateInfo.getForcedTitle(activity) : updateInfo.getTitle(activity))
                         .setMessage(forced ? updateInfo.getForcedDescription(activity) : updateInfo.getDescription(activity))
-                        .setPositiveButton(updateInfo.getAcceptString(activity), null)
-                        .setCancelable(!forced);
+                        .setCancelable(!forced)
+                        .setPositiveButton(updateInfo.getAcceptString(activity), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                FreshAir.goToUpdatePage(activity);
 
+                                // If we're forcing an update, reshow this dialog so it stays on top
+                                if (forced) {
+                                    execute(activity);
+                                }
+                            }
+                        });
+
+                // Only show the decline button if we aren't forcing the update
                 if (!forced) {
                     dialogBuilder.setNegativeButton(updateInfo.getDeclineString(activity), null);
                 }
@@ -45,13 +63,35 @@ public class FreshAir {
         }, forced);
     }
 
+    public static boolean goToUpdatePage(Context context) {
+        final Uri playStoreUri = Uri.parse("market://details?id=" + context.getPackageName());
+        FreshAirLog.i("Attempting to open Play Store update page at URI: " + playStoreUri.toString());
+
+        final Intent playStoreIntent = new Intent(Intent.ACTION_VIEW, playStoreUri);
+        final PackageManager packageManager = context.getPackageManager();
+
+        for (ResolveInfo resolveInfo : packageManager.queryIntentActivities(playStoreIntent, 0)) {
+            final ActivityInfo resolveActivity = resolveInfo.activityInfo;
+            final String resolvePackageName = resolveActivity.packageName;
+            if (resolvePackageName.equals("com.android.vending")) {
+                FreshAirLog.i("Opening Play Store update page...");
+                playStoreIntent.setComponent(new ComponentName(resolvePackageName, resolveActivity.name));
+                context.startActivity(playStoreIntent);
+                return true;
+            }
+        }
+
+        FreshAirLog.e("Failed to find the Play Store as a candidate for the resolution of update URI: " + playStoreUri.toString());
+        return false;
+    }
+
     public static void showOnboarding(FragmentActivity activity) {
 
         MainPreferences preferences = new MainPreferences(activity);
         final int lastPromptVersion = preferences.getLastOnboardingPromptVersion();
         final int releaseInfoVersion = releaseInfo.getVersionCode();
 
-        FreshAirLog.v( "Last Onboarding Prompt: " + lastPromptVersion + " Attempting to display release: " + releaseInfoVersion);
+        FreshAirLog.v("Last Onboarding Prompt: " + lastPromptVersion + " Attempting to display release: " + releaseInfoVersion);
 
         if (lastPromptVersion < releaseInfo.getVersionCode()) {
             OnboardingFragment onboardingFragment = OnboardingFragment.newInstance(releaseInfo);
